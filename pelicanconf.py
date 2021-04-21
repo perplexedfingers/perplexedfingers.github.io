72483eca4641f5bc97f9d5d27cd2201326dc6398
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2021-04-22 02:20:37 +0800

    Remove links. Correct `SITEURL`. Set `THEME` and `DEFAULT_DATE_FORMAT`

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,14 @@
 
 AUTHOR = 'perplexedfingers'
 SITENAME = 'Blog'
-SITEURL = ''
+SITEURL = 'https://perplexedfingers.github.io'
+
+THEME = 'nmnlist'
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Taipei'
-
-DEFAULT_LANG = 'zh'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,16 +20,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
